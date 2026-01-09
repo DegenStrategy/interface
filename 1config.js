3ab03f9e244656f4f0e64964cc7d5dd2a9634140
DegenStrategy <denisordel2@gmail.com>
2026-01-09 01:06:50 +0100

    Update API URL and enhance error handling

--- 1config.js
+++ 1config.js
@@ -1,6 +1,6 @@
 //simply replace API url and use the governance panel on your local device
 var isTestnet = false
-var apiURL = "http://oink-pls.duckdns.org/"; //for proposals, votes.
+var apiURL = "https://api.oink-pls.com/"; //for proposals, votes.
 var rpcLink = "https://rpc-pulsechain.g4mm4.io"
 
 var provider
@@ -150,5 +150,6 @@ style.textContent = `
 `;
 
 document.head.appendChild(style);
+
 
 
